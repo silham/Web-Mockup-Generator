0eadbcfc25b51dbb7d6563a7119a978c30ec1b58
silham <ilhamshakil@gmail.com>
2024-05-10 23:06:11 +0530

    added aws chrome

--- app/api/screenshot/route.ts
+++ app/api/screenshot/route.ts
@@ -1,26 +1,39 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import puppeteer from 'puppeteer';
+import chromium from "chrome-aws-lambda";
 
 export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url)
-  const url = searchParams.get('url')
-  const width = searchParams.get('width')
-  const height = searchParams.get('height')
+  const { searchParams } = new URL(req.url);
+  const url = searchParams.get('url');
+  const width = searchParams.get('width');
+  const height = searchParams.get('height');
 
   if (!url || !width || !height) {
     return new Response('Missing params', { status: 400 });
   }
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.setViewport({width: Number(width), height: Number(height)});
-  const screenshot = await page.screenshot({ encoding: 'binary' });
 
-  await browser.close();
+  // Ensure Lambda Layer is configured with Chromium binary
+  const executablePath = await chromium.executablePath;
 
-  return new Response(screenshot, {
-    headers: {
-      'Content-Type': 'image/png',
-    },
-  });
+  try {
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      headless: true, // Recommended for serverless environments
+      executablePath,
+    });
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    await page.setViewport({ width: Number(width), height: Number(height) });
+    const screenshot = await page.screenshot({ type: 'png' }); // More concise
+
+    await browser.close();
+
+    return new Response(screenshot, {
+      headers: {
+        'Content-Type': 'image/png',
+      },
+    });
+  } catch (error) {
+    console.error('Error generating screenshot:', error);
+    return new Response('Failed to generate screenshot', { status: 500 });
+  }
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    externals: ['chrome-aws-lambda'],
+};
 
 export default nextConfig;
