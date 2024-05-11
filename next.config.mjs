f97887b59e69ddc6a1f533fedf6e216c708b12da
silham <ilhamshakil@gmail.com>
2024-05-11 12:46:20 +0530

    added stream-browserify for debugging

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.resolve.fallback = {
+            ...config.resolve.fallback,
+            stream: require.resolve('stream-browserify'),
+          };
+        }
+        return config;
+      },
+};
 
 export default nextConfig;
